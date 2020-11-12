6044b9499fd52e28bdc74ba7b402a24667450f7c
EliChang <eliflichang@gmail.com>
2020-11-13 00:06:54 +0800

    first commit

---
+++ app/src/main/java/com/eli/ali/TaobaoAct.java
@@ -0,0 +1,14 @@
+package com.eli.ali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaobaoAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taobao);
+    }
+}
---
+++ app/src/main/java/com/eli/ali/ZhifubaoAct.java
@@ -0,0 +1,14 @@
+package com.eli.ali;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ZhifubaoAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhifubao);
+    }
+}
